79060ba6694b4973971f1ea99b4af3ba7e94f564
odrzewicki <u64723@student.wat.edu.pl>
2025-04-15 11:00:06 +0200

    Add files via upload

---
+++ utils/model.py
@@ -0,0 +1,6 @@
+users:list=[
+    {'name':'Bartlomiej','location':'Lublin','posts':2},
+    {'name':'Oliwier','location':'Zamość','posts':3},
+    {'name':'Kuba','location':'Warszawa','posts':500},
+    {'name':'Konrad','location':'Lublin','posts':10},
+]
